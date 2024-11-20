802ffedfe54a8d36be37f9679c7666e51f27ff57
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-11-20 18:30:55 +0200

    new commit

--- chat-app/src/app/main/chat/chat.routing.ts
+++ chat-app/src/app/main/chat/chat.routing.ts
@@ -1,6 +1,5 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 import { EmptyMessageComponent } from './empty-message/empty-message.component';
-import { ChatComponent } from './chat.component';
 import { MessageComponent } from './message/message.component';
 
 export const ChatRoutes: Routes = [
--- chat-app/src/app/main/setting/setting.routing.ts
+++ chat-app/src/app/main/setting/setting.routing.ts
@@ -1,6 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
+import { SettingComponent } from './setting.component';
 
 export const SettingRoutes: Routes = [
-  {  },
+  { path:'',component:SettingComponent },
 ];
 
