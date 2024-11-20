9d0370b342e2371c9fafd4808d52eb0f21dd7190
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-11-20 21:22:41 +0200

    new commit

--- chat-app/src/app/main/chat/empty-message/empty-message.component.ts
+++ chat-app/src/app/main/chat/empty-message/empty-message.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   standalone:true,
-  imports:[MatIconModule],
+  imports:[MatIconModule,MatButtonModule, MatMenuModule],
   selector: 'app-empty-message',
   templateUrl: './empty-message.component.html',
   styleUrls: ['./empty-message.component.css']
 })
 export class EmptyMessageComponent implements OnInit {
-
+  private selectedFileName: string = '';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFileName = input.files[0].name; // Update the file name
+    }
   }
 
 }
