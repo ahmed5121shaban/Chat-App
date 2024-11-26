45dbde2459a2ac03e2beefb5d5843a84743f6699
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-11-26 09:39:58 +0200

    setting component

--- chat-app/src/app/main/setting/setting.component.ts
+++ chat-app/src/app/main/setting/setting.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-setting',
   standalone:true,
+  imports:[MatIconModule],
   templateUrl: './setting.component.html',
   styleUrls: ['./setting.component.css']
 })
