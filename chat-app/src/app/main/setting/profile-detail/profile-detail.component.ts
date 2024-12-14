97b30d6df2e55c42efc0a324d0a3f48f1f1ca78c
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-12-14 10:24:28 +0200

    profile detail component added

---
+++ chat-app/src/app/main/setting/profile-detail/profile-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone:true,
+  imports:[],
+  selector: 'app-profile-detail',
+  templateUrl: './profile-detail.component.html',
+  styleUrls: ['./profile-detail.component.css']
+})
+export class ProfileDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- chat-app/src/app/main/setting/setting.routing.ts
+++ chat-app/src/app/main/setting/setting.routing.ts
@@ -1,8 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { SettingComponent } from './setting.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
 
 export const SettingRoutes: Routes = [
+  { path:'',component:ProfileDetailComponent },
   { path:'edit-profile',component:EditProfileComponent },
 ];
 
